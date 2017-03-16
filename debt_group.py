a125125234e43250c967593837342ce8a74d8934
yajun <2374712387@qq.com>
2017-03-16 10:19:59 +0800

    add AA and AA-

--- debt_group.py
+++ debt_group.py
@@ -17,17 +17,34 @@ AA_201516 = df[df['信用评级']=='AA']['公司名称']
 AA_minus_201516 = df[df['信用评级']=='AA-']['公司名称']
 AA_plus_201516 = df[df['信用评级']=='AA+']['公司名称']
 AA_201516 = AA_201516.drop_duplicates()
-AA__2016 = df[(df['发布日期']>='2016-01-01')&(df['信用评级']=='AA-')]
+AA_minus_2016 = df[(df['发布日期']>='2016-01-01')&(df['信用评级']=='AA-')]
 
 record2016 = df[df['发布日期']>='2016-01-01']
 record2015 = df[df['发布日期']<'2016-01-01']
+
 AA15=record2015[record2015['信用评级']=='AA']
 NAA15 = record2015[record2015['信用评级']!='AA']
+AA15_merge=AA15.merge(NAA15,how='left',on='公司名称').fillna(0)
+AA15_only=AA15_merge[AA15_merge['信用评级_y']==0]['公司名称'].drop_duplicates()
+AA15_only.to_csv('AA15_only.csv')
 
- AA15.merge(NAA15,on='公司名称')
- 
-AA15.join(NAA15,lsuffix='_caller', rsuffix='_other',how='inner',on='公司名称')
-AA15.merge(NAA15)
-AA15
-NAA15
-AA15.merge(NAA15,on='公司名称')
+
+AA1516 = df[df['信用评级']=='AA']
+NAA1516 = df[df['信用评级']!='AA']
+AA1516_merge=AA1516.merge(NAA1516,how='left',on='公司名称').fillna(0)
+AA_only1516=AA1516_merge[AA1516_merge['信用评级_y']==0]['公司名称'].drop_duplicates()
+AA_only1516.to_csv('AA_only1516.csv')
+
+AA16=record2016[record2016['信用评级']=='AA']
+NAA16 = record2016[record2016['信用评级']!='AA']
+AA16_merge=AA16.merge(NAA16,how='left',on='公司名称').fillna(0)
+AA16_only=AA16_merge[AA16_merge['信用评级_y']==0]['公司名称'].drop_duplicates()
+AA16_only.to_csv('AA16_only.csv')
+
+AA_16=record2016[record2016['信用评级']=='AA-']
+NAA_16 = record2016[record2016['信用评级']!='AA-']
+AA_16_merge=AA_16.merge(NAA_16,how='left',on='公司名称').fillna(0)
+AA_16_only=AA_16_merge[AA_16_merge['信用评级_y']==0]['公司名称'].drop_duplicates()
+AA_16_only.to_csv('AA_16_only.csv')
+
+
