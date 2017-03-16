a7556a4530dc3b443ee1ce5fe583ffd0b28ad23e
yajun <2374712387@qq.com>
2017-03-16 10:22:10 +0800

    # 15 AA AND 16 AA- # 15 AA AND 16 AA

--- debt_group.py
+++ debt_group.py
@@ -35,16 +35,31 @@ AA1516_merge=AA1516.merge(NAA1516,how='left',on='公司名称').fillna(0)
 AA_only1516=AA1516_merge[AA1516_merge['信用评级_y']==0]['公司名称'].drop_duplicates()
 AA_only1516.to_csv('AA_only1516.csv')
 
+#16 only AA
 AA16=record2016[record2016['信用评级']=='AA']
 NAA16 = record2016[record2016['信用评级']!='AA']
 AA16_merge=AA16.merge(NAA16,how='left',on='公司名称').fillna(0)
 AA16_only=AA16_merge[AA16_merge['信用评级_y']==0]['公司名称'].drop_duplicates()
 AA16_only.to_csv('AA16_only.csv')
 
+# 16 only AA-
 AA_16=record2016[record2016['信用评级']=='AA-']
 NAA_16 = record2016[record2016['信用评级']!='AA-']
 AA_16_merge=AA_16.merge(NAA_16,how='left',on='公司名称').fillna(0)
 AA_16_only=AA_16_merge[AA_16_merge['信用评级_y']==0]['公司名称'].drop_duplicates()
 AA_16_only.to_csv('AA_16_only.csv')
+
+# 15 AA AND 16 AA-
+AA15_AA_16=AA15.merge(AA_16,how='left',on='公司名称').fillna(0)
+AA15_AA_16_only=AA15_AA_16[AA15_AA_16['信用评级_y']=='AA-']['公司名称'].drop_duplicates()
+AA15_AA_16_only.to_csv('AA15_AA_16_only.csv')
+
+
+# 15 AA AND 16 AA
+AA15_AA16=AA15.merge(AA16,how='left',on='公司名称').fillna(0)
+AA15_AA16_only=AA15_AA16[AA15_AA16['信用评级_y']=='AA']['公司名称'].drop_duplicates()
+AA15_AA16_only.to_csv('AA15_AA16_only.csv')
+
+
 
 
