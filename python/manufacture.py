be5295c39a18f7bbcc935b57736c17c8d11dc875
Zheng Yajun <2374712387@qq.com>
2017-05-17 16:40:00 +0800

    fixed NN ROC

--- python/manufacture.py
+++ python/manufacture.py
@@ -9,18 +9,15 @@ import pandas as pd
 import numpy  as np
 from pymongo import *
 from numpy import *
-
+from random import shuffle
 from sklearn.datasets import load_iris
  
 #导入IRIS数据集
 iris = load_iris()
-
 #特征矩阵
 iris.data
-
 #目标向量
 iris.target
-
 #import json
 #from bson.son import SON
 #
@@ -73,13 +70,11 @@ result.pop('code')
 #from sklearn.preprocessing import OneHotEncoder 
 #OneHotEncoder().fit_transform(result['PROVINCE'])
 
-
-
 from sklearn.feature_selection import VarianceThreshold
 #方差选择法，返回值为特征选择后的数据
 #参数threshold为方差的阈值
 VarianceThreshold(threshold=3).fit_transform(iris.data)
-Variance = VarianceThreshold(threshold=1).fit_transform(X)
+#Variance = VarianceThreshold(threshold=1).fit_transform(X)
 
 #StandardScaler.fit_transform(iris.data)
 #query = db.default.distinct('发行人')
@@ -87,8 +82,9 @@ Variance = VarianceThreshold(threshold=1).fit_transform(X)
 #set(dfcomp[0])
 
 def LogisticRegression(result):
-    dd = pd.DataFrame(Variance)
-    dd['flag'] = df_flag
+#    dd = pd.DataFrame(Variance)
+    dd = result
+#    dd['flag'] = df_flag
     
     from random import shuffle
     data = dd.as_matrix()
@@ -115,33 +111,11 @@ def LogisticRegression(result):
     lr.fit(x, y) # 用筛选后的特征数据来训练模型
     print(u'训练结束')
     print(u'平均正确率为：%s' % lr.score(x, y))
-
-#构建LM神经网络模型
-def LM_NN():
-    from keras.models import Sequential #导入神经网络初始化函数
-    from keras.layers.core import Dense, Activation #导入神经网络层函数、激活函数
-    netfile = '../tmp/net.model' #构建的神经网络模型存储路径
-    
-    net = Sequential() #建立神经网络
-    net.add(Dense(input_dim = n-1, output_dim = 100)) #添加输入层（3节点）到隐藏层（10节点）的连接
-    net.add(Activation('relu')) #隐藏层使用relu激活函数
-    net.add(Dense(input_dim = 100, output_dim = 5)) #添加输入层（3节点）到隐藏层（10节点）的连接
-    net.add(Activation('relu')) #隐藏层使用relu激活函数
-    net.add(Dense(input_dim = 5, output_dim = 1)) #添加隐藏层（10节点）到输出层（1节点）的连接
-    net.add(Activation('sigmoid')) #输出层使用sigmoid激活函数
-    net.compile(loss = 'binary_crossentropy', optimizer = 'adam', class_mode = "binary") #编译模型，使用adam方法求解
-    m,n = np.shape(train)
-    net.fit(train[:,:n-1], train[:,n-1], nb_epoch=1000, batch_size=1) #训练模型，循环1000次
-    net.save_weights(netfile) #保存模型
-    
-    predict_result = net.predict_classes(train[:,:n-1]).reshape(len(train)) #预测结果变形
-    '''这里要提醒的是，keras用predict给出预测概率，predict_classes才是给出预测类别，而且两者的预测结果都是n x 1维数组，而不是通常的 1 x n'''
 from cm_plot import * #导入自行编写的混淆矩阵可视化函数
-def comp_plot():
-
+def comp_plot(n,predict_result):
     cm_plot(train[:,n-1], predict_result).show() #显示混淆矩阵可视化结果
 
-def plot_roc():
+def plot_roc(n,net, test):
     from sklearn.metrics import roc_curve #导入ROC曲线函数
     import matplotlib.pyplot as plt
     predict_result = net.predict(test[:,:n-1]).reshape(len(test))
@@ -153,6 +127,37 @@ def plot_roc():
     plt.xlim(0,1.05) #边界范围
     plt.legend(loc=4) #图例
     plt.show() #显示作图结果
+
+#构建LM神经网络模型
+#def LM_NN(result):
+from keras.models import Sequential #导入神经网络初始化函数
+from keras.layers.core import Dense, Activation #导入神经网络层函数、激活函数
+#    dd = pd.DataFrame(Variance)
+dd = result
+#    dd['flag'] = df_flag
+data = dd.as_matrix()
+shuffle(data)
+p = 0.8 # train/test ratio
+m,n = shape(data)
+train = data[:int(m*p),:]
+test = data[int(m*p):,:]
+netfile = '../tmp/net.model' #构建的神经网络模型存储路径
+
+net = Sequential() #建立神经网络
+net.add(Dense(input_dim = n-1, output_dim = 100)) #添加输入层（3节点）到隐藏层（10节点）的连接
+net.add(Activation('relu')) #隐藏层使用relu激活函数
+net.add(Dense(input_dim = 100, output_dim = 5)) #添加输入层（3节点）到隐藏层（10节点）的连接
+net.add(Activation('relu')) #隐藏层使用relu激活函数
+net.add(Dense(input_dim = 5, output_dim = 1)) #添加隐藏层（10节点）到输出层（1节点）的连接
+net.add(Activation('sigmoid')) #输出层使用sigmoid激活函数  softmax: posibility
+net.compile(loss = 'binary_crossentropy', optimizer = 'adam', class_mode = "binary") #编译模型，使用adam方法求解
+m,n = np.shape(train)
+net.fit(train[:,:n-1], train[:,n-1], nb_epoch=1000, batch_size=1) #训练模型，循环1000次
+net.save_weights(netfile) #保存模型
+predict_result = net.predict_classes(train[:,:n-1]).reshape(len(train)) #预测结果变形
+comp_plot(n,predict_result)
+plot_roc(n,net,test)
+'''这里要提醒的是，keras用predict给出预测概率，predict_classes才是给出预测类别，而且两者的预测结果都是n x 1维数组，而不是通常的 1 x n'''
 
 def sigmoid(inX):
     return float(1.0/(1+exp(-inX)))
@@ -253,6 +258,8 @@ if __name__ == "__main__":
     scaler = StandardScaler
     
     tSet = result
+    
+'''
     probs = result.drop(labels=['df'], axis=1)
     X = StandardScaler().fit_transform(probs.values)
 #    scaler.fit_transform(probs)
@@ -306,7 +313,7 @@ if __name__ == "__main__":
     problist = list()
     for line in probs.values:
         problist.append(prob(line.astype('float64'), weights))
-
+'''
 #    ii = 0
 #    for a in problist:
 #        if a!=0.0:
